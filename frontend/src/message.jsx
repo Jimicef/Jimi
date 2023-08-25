34369676135bbc2a3b47a1f4fb61d574033901a9
coddingyun <igy2840@gmail.com>
2023-08-25 14:16:06 +0900

    Feat: chat ui 구현

---
+++ frontend/src/message.jsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import {Box, TextField, Button, Typography, Avatar, Grid, Paper} from "@mui/material";
+
+export const Message = ({ message }) => {
+    const isBot = message.sender === "bot";
+  
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: isBot ? "flex-start" : "flex-end",
+          mb: 2,
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: isBot ? "row" : "row-reverse",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ bgcolor: isBot ? "primary.main" : "secondary.main" }}>
+            {isBot ? "B" : "U"}
+          </Avatar>
+          <Paper
+            variant="outlined"
+            sx={{
+              p: 2,
+              ml: isBot ? 1 : 0,
+              mr: isBot ? 0 : 1,
+              backgroundColor: isBot ? "primary.light" : "secondary.light",
+              borderRadius: isBot ? "20px 20px 20px 5px" : "20px 20px 5px 20px",
+            }}
+          >
+            <Typography variant="body1">{message.text}</Typography>
+          </Paper>
+        </Box>
+      </Box>
+    );
+  };
