075971773b0feb9cf7da1a9e654bb10ac5034155
chjung99 <cksgh1168@gmail.com>
2023-08-25 14:12:47 +0900

    add jimi.py test

---
+++ backend/jimi.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from typing import Dict
+
+app = FastAPI()
+
+@app.post("/api/qa")
+async def get_answer(question:str) -> Dict[str, str]:
+    
+    answer = "42"
+    support = "Don't panic and always carry a towel."
+    response = {"answer": answer, "support": support}
+    return response
