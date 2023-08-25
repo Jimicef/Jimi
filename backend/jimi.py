d2c831816a6cbfb727ebf7db1ba9319ce87c318c
chjung99 <cksgh1168@gmail.com>
2023-08-25 16:55:54 +0900

    test

--- backend/jimi.py
+++ backend/jimi.py
@@ -14,3 +14,10 @@ async def get_answer(question:str) -> Dict[str, str]:
     support = "Don't panic and always carry a towel."
     response = {"answer": answer, "support": support}
     return response
+
+@app.post("/api/test")
+async def get_answer2(question:int):
+    
+    answer = question
+    
+    return answer
