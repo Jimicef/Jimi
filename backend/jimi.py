fdd3f8487d00349a8a0af4e34fd5aa90c5de5cba
chjung99 <cksgh1168@gmail.com>
2023-08-25 23:35:01 +0900

    fix cors

--- backend/jimi.py
+++ backend/jimi.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from typing import Dict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:80",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 async def root():
@@ -14,10 +29,3 @@ async def get_answer(question:str) -> Dict[str, str]:
     support = "Don't panic and always carry a towel."
     response = {"answer": answer, "support": support}
     return response
-
-@app.post("/api/test")
-async def get_answer2(question:int):
-    
-    answer = question
-    
-    return answer
