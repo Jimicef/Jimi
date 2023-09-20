cfac2f4f65eb8461bd1115f5a42cf9b8cdde450d
chjung99 <cksgh1168@gmail.com>
2023-09-20 16:38:56 +0900

    inject dependecies test

---
+++ backend/handlers.py
@@ -0,0 +1,122 @@
+from fastapi import FastAPI, Query, UploadFile, Depends
+from bs4 import BeautifulSoup
+import requests
+
+async def get_service_list(keyword : str = Query(None,description = "검색 키워드"),
+                           count : int = Query(0,description = "페이지 번호"),
+                           chktype1 : str = Query(None,description = "서비스 분야"),
+                           siGunGuArea : str = Query(None,description = "시/군/구 코드"),
+                           sidocode : str = Query(None,description = "시/도 코드"),
+                           svccd : str = Query(None,description = "사용자 구분"),
+                           ):
+    url = "https://www.gov.kr/portal/rcvfvrSvc/svcFind/svcSearchAll"
+    
+    div_count = count // 2
+    
+    last_page = False
+
+    params = {
+        "siGunGuArea" : siGunGuArea,
+        "sidocode" : sidocode,
+        'svccd' : svccd,
+        "chktype1" : chktype1,
+        "startCount": 12*div_count,
+        "query": keyword
+    }
+    response = requests.get(url,params=params)
+
+
+    if response.status_code == 200:
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+    else : 
+        return response.status_code
+    
+    
+    option_tag = soup.select_one('#orgSel option')
+    text = option_tag.text  # '전체 (9,880)'
+
+    # 괄호 안의 숫자를 추출
+    result_count = int(''.join(filter(str.isdigit, text)))
+    if result_count == 0:
+        return {
+        "answer" : None,
+        "support" : None,
+        "lastpage" : True
+        }
+    
+    page_count = (result_count - 1) // 12
+
+    if result_count == 0:
+        last_page = True
+    elif div_count  == page_count :
+        # 첫페이지가 last인 경우
+        if count % 2 == 0 and result_count - (6 * count) <= 6:
+            last_page = True
+        # 두번째 페이지가 last인 경우
+        elif count % 2 !=0 and result_count - (6 * count) <= 6:
+            last_page = True
+
+    card_data_list = []
+    cards = soup.find_all('div', class_='card-item')
+
+
+    for card in cards:
+        card_tag = soup.find('div', class_='card-tag')
+        department = card_tag.find('em', class_='chip').text
+        title = card.find('a', class_='card-title')
+        card_title = title.text
+        card_id = title.get('href').split('/')[4].split('?')[0]
+        card_desc = card.find('p', class_='card-desc').text
+        card_info_list = card.find_all('li', class_='card-list')
+
+        card_info = {
+            "institution" : department,
+            "serviceId" : card_id,
+            "title" : card_title,
+            "description" : card_desc
+        }
+        for info in card_info_list:
+            try:
+                strong_text = info.find('strong', class_='card-sub').text
+            except:
+                strong_text = None
+            try:
+                card_text = info.find('span', class_='card-text').text
+            except:
+                card_text = None
+
+            if strong_text.split()[0] == "신청기간":
+                card_info["dueDate"] = card_text
+            elif strong_text.split()[0] == "접수기관":
+                card_info["rcvInstitution"] = card_text
+            elif strong_text.split()[0] == "전화문의":
+                card_info["phone"] = card_text
+            elif strong_text.split()[0] == "지원형태":
+                card_info["format"] = card_text
+                
+
+        if len(card_info.keys()) > 6:
+            card_data_list.append(card_info)
+        else:
+            break
+    if count % 2 == 0:
+        card_data_list = card_data_list[:6]
+    else :
+        card_data_list = card_data_list[6:]
+    # request = requests.Request('GET', url, params=params)
+    # prepared_request = request.prepare()
+
+    # # 최종 요청되는 URL 확인
+    # final_url = prepared_request.url
+    if keyword:
+        message = f"{keyword}에 대한 {result_count}개의 통합검색 결과입니다."
+    else:
+        message = f"선택한 조건에 대한 {result_count}개의 통합검색 결과입니다."
+    
+    return {
+        "answer" : message,
+        "support" : card_data_list,
+        "lastpage" : last_page
+    }
