5a5658b28ebad3efdaecac7a271b44f24f025499
coddingyun <igy2840@gmail.com>
2023-09-23 21:52:01 +0900

    Feat: region2가 전체일 경우 처리

--- frontend/src/Voice.js
+++ frontend/src/Voice.js
@@ -357,10 +357,9 @@ const Voice = () => {
                     // 첫 번째 아이템을 스페이스로 분할
                     const [region1, region2] = firstItem.split(' ');
 
-                    if (region1 === region2) {
+                    if (region1 === region2 || region2 === '전체') {
                     // 앞과 뒤의 내용이 같으면 해당 지역의 정보를 가져와서 결과에 추가
                         const regionInfo = subRegionList[region1];
-                        
                         if (regionInfo) {
                             const subRegions = regionInfo.map(obj => Object.keys(obj)[0]);
                             // "서울특별시"를 앞에 붙여서 결과 어레이에 추가
@@ -456,7 +455,6 @@ const Voice = () => {
                         })
                         .then(response => response.json())
                         .then(data => {
-                            
                             dispatch({
                                 type: SET_VOICE_COUNT,
                                 data: 0
