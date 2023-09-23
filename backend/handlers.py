c05cd36e77c0777fc502642b8289482dca4cbc84
chjung99 <cksgh1168@gmail.com>
2023-09-23 21:22:40 +0900

    fix query dsl

--- backend/handlers.py
+++ backend/handlers.py
@@ -449,13 +449,14 @@ async def post_opensearch_service_list(data: Annotated[dict,{
     low_query = {
         "query": {
             "bool": {
-            "must": [
-                { "terms": { "소관기관명.keyword": data['sidocode'] } }
-            ],
+            # "must": [
+                
+            # ],
             "should": [
                 { "match": { "서비스명": data['keyword'] } },
                 { "terms": { "사용자구분.keyword": data['svccd'] } },
                 { "terms": { "서비스분야.keyword": data['chktype1'] } },#["생활안정","주거·자립"] 이렇게 줘야함
+                { "terms": { "소관기관명.keyword": data['sidocode'] } }
             ]
             }
         }
