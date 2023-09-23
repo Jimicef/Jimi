16719de41985619ad624c4554b0f2e4258a1268c
chjung99 <cksgh1168@gmail.com>
2023-09-24 01:01:37 +0900

    fix

--- backend/handlers.py
+++ backend/handlers.py
@@ -404,9 +404,10 @@ async def post_voice_chat(file: UploadFile, history: UploadFile):
             #     get_service_params['siGunGuArea'] = sub_region_code[get_service_params['sidocode']][get_service_params['siGunGuArea']]
             # except:
             #     get_service_params['siGunGuArea'] = sub_region_code[get_service_params['sidocode']]["전체"]
-            get_service_params['sidocode'] = [get_service_params['sidocode']+" "+get_service_params['siGunGuArea']]
+            
             # get_service_params['chktype1'] = ["생활안정", "주거·자립", "보육·교육", "고용·창업", "보건·의료", "행정·안전", "임신·출산", "보호·돌봄", "문화·환경", "농림축산어업"]
             try:
+                get_service_params['sidocode'] = [get_service_params['sidocode']+" "+get_service_params['siGunGuArea']]
                 get_service_params['chktype1'] = [get_service_params['chktype1']]
             except:
                 print(get_service_params)
